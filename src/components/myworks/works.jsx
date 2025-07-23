508d85940a08f6e895b15eb419eb56bd35c8dec3
Laxmi Prasad <s.laxmiprasad2006@gmail.com>
2025-07-23 15:07:25 +0530

    project done

--- src/components/myworks/works.jsx
+++ src/components/myworks/works.jsx
@@ -12,21 +12,21 @@ const Myworks = () => {
           <p>
             Farm Link is a web application that connects farmers with customers, allowing them to sell their produce directly. This is build by using the Base44 AI. Application has two interfaces one for farmer and the other for customers.
           </p>
-          <p className="prolink"><span className="linkboldtxt">Link: </span>app--farm-link-copy-56f4226a.base44.app</p>
+          <p className="prolink"><span className="linkboldtxt">Link: </span><a href="https://app--farm-link-copy-56f4226a.base44.app/" target="blank">app--farm-link-copy-56f4226a.base44.app</a></p>
         </div>
         <div className="project">
           <h2 className="projectname">Grocers Galaxy</h2>
           <p>
             Grocers Galaxy is a web application that allows users to order groceries online. It has a user-friendly interface. The application is built using Angular. This application also has two interfaces one for admin to manage the products and the other for customers to place orders.
           </p>
-          <p className="prolink"><span className="linkboldtxt">Git Link: </span>github.com/laxmiprasads/Grocers-Galaxy</p>
+          <p className="prolink"><span className="linkboldtxt">Git Link: </span><a href="https://github.com/laxmiprasads/Grocers-Galaxy" target="blank">github.com/laxmiprasads/Grocers-Galaxy</a></p>
         </div>
         <div className="project">
           <h2 className="projectname">Optisight</h2>
           <p>OptiSight eye hospital website,built by using HTML, CSS, Bootstrap. It has different sections 
             like diseases, food care, blogs. The website is responsive and has a clean design.
           </p>
-          <p className="prolink"><span className="linkboldtxt">Link: </span>slpoptisight.niat.tech</p>
+          <p className="prolink"><span className="linkboldtxt">Link: </span><a href="https://slpoptisight.niat.tech/" target="blank">slpoptisight.niat.tech</a></p>
         </div>
       </div>
     </div>
